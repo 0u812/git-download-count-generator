03c3c27d39996607d224f2cb8c9c4d1e2fb5acbd
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2016-03-25 05:51:22 +0530

    change to handle 404 repository not found

--- git-download-status.py
+++ git-download-status.py
@@ -1,14 +1,13 @@
 #!/usr/local/bin/python
-# Tool to convert CSV files (with configurable delimiter and text wrap
-# character) to Excel spreadsheets.
 import xlsxwriter
 import json
 
-from urllib2 import Request, urlopen, URLError
+from urllib2 import Request, urlopen, URLError, HTTPError
 
+user = raw_input("Enter User name: ")
 project = raw_input("Enter project name: ")
-request = Request('https://api.github.com/repos/wso2/'+project+'/releases')
-workbook = xlsxwriter.Workbook('exports/'+project+'.xlsx')
+request = Request('https://api.github.com/repos/'+user+'/'+project+'/releases')
+workbook = xlsxwriter.Workbook('exports/'+user+'-'+project+'.xlsx')
 worksheet = workbook.add_worksheet(project)
 bold = workbook.add_format({'bold': True})
 worksheet.write('A1', 'Releases', bold)
@@ -17,15 +16,21 @@ row = 1
 col = 0
 
 try:
-	response = urlopen(request)
-	download_counts = json.loads(response.read())
-finally:
-	for item in download_counts:
-		if 'tag_name' in item:
-			worksheet.write(row, col, item['tag_name'])
-			if item['assets']:
-				worksheet.write(row, col + 1, item['assets'][0]['download_count'])
-			row += 1
-			continue
+	resp = urlopen(request)
+except HTTPError as e:
+	if e.code == 404:
+		print "No repository available"
+		worksheet.write(row, col, "No repository available")
+	else:
+		# 200
+		download_counts = json.loads(response.read())
+		for item in download_counts:
+			if 'tag_name' in item:
+				worksheet.write(row, col, item['tag_name'])
+				if item['assets']:
+					worksheet.write(row, col + 1, item['assets'][0]['download_count'])
+				row += 1
+				continue
 
-workbook.close()
+finally:
+	workbook.close()
